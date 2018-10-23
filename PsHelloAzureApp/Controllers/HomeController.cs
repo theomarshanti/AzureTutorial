96579e5836768bd384a62d72ea5a906c6141f588
Test <omarshantirn@gmail.com>
2018-10-23 13:22:32 -0500

    Added a setting and updated Home

--- PsHelloAzureApp/Controllers/HomeController.cs
+++ PsHelloAzureApp/Controllers/HomeController.cs
@@ -5,14 +5,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PsHelloAzureApp.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace PsHelloAzureApp.Controllers
 {
     public class HomeController : Controller
     {
+        IConfiguration configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = this.configuration["Greeting"];
+            return View("Index", model);
         }
 
         public IActionResult About()
